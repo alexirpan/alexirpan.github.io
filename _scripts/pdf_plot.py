af1bdda1b73dcfa3ee8c1919ef3bc6394e0f5d94
Alex Irpan <xelanapri@gmail.com>
2015-09-05 12:36:14 -1000

    More figure creation cleaning up

--- _scripts/pdf_plot.py
+++ _scripts/pdf_plot.py
@@ -15,15 +15,15 @@ def geometric(p):
     probs = [(z == i).sum() / float(SAMPLES) for i in xrange(11)]
     width = 1.0
 
+    plt.figure(facecolor='white')
     plt.bar(np.arange(0, 11 * width, width), probs, width, align='center', alpha=0.5)
 
-    plt.xlabel('x')
-    plt.ylabel('P[X=x]')
+    plt.xlabel(r'$x$', fontsize=16)
+    plt.ylabel(r'$P[X=x]$', fontsize=16)
     plt.xticks(np.arange(11))
     plt.xlim((-width / 2, 10 + width / 2))
     plt.ylim((0, 0.55))
-    plt.title("Probability mass function for geometric distribution, p = %.2f" % p)
-    plt.tight_layout()
+    plt.title(r"Probability mass function for geometric distribution, $p$ = %.1f" % p)
     plt.show()
 
 def std_normal():
@@ -31,11 +31,12 @@ def std_normal():
     x = np.linspace(-3, 3, num=1000)
     y = (2 * np.pi) ** -0.5 * np.exp(-x*x/2)
 
+    plt.figure(facecolor='white')
     plt.plot(x, y)
 
     plt.title("Probability density function for standard normal")
-    plt.xlabel('x')
-    plt.ylabel('pdf(x)')
+    plt.xlabel(r'$x$')
+    plt.ylabel(r'$p.d.f.(x)$')
     plt.ylim((0, 0.45))
     ax = plt.gca()
     ax.fill_between(x, y, facecolor='blue', alpha=0.5)
