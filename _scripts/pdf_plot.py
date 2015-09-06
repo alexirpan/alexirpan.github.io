10873b5e4abafd5c5d97b449d1e82a76541d6704
Alex Irpan <xelanapri@gmail.com>
2015-09-05 21:20:31 -1000

    Add decision boundary to plot code

--- _scripts/pdf_plot.py
+++ _scripts/pdf_plot.py
@@ -1,7 +1,10 @@
 """
-Make plots for p.d.f. or p.m.f. of various distribution
+Make plots for p.d.f. or p.m.f. of various distribution. Used for the
+envelopes post.
 
-Requires NumPy + matplotlib + possibly SciPy + a LaTeX distribution
+Lots of magic constants here.
+
+Requires NumPy + matplotlib + a LaTeX distribution
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,13 +21,20 @@ def geometric(p):
     plt.figure(facecolor='white')
     plt.bar(np.arange(0, 11 * width, width), probs, width, align='center', alpha=0.5)
 
+    plt.title(r"Probability mass function for geometric distribution, $p$ = %.1f" % p)
     plt.xlabel(r'$x$', fontsize=16)
     plt.ylabel(r'$P[X=x]$', fontsize=16)
     plt.xticks(np.arange(11))
     plt.xlim((-width / 2, 10 + width / 2))
     plt.ylim((0, 0.55))
-    plt.title(r"Probability mass function for geometric distribution, $p$ = %.1f" % p)
+    # Decision boundary
+    plt.axvline(x=4-width/2, color='r')
+    plt.text(4-width/2+0.1, 0.4, r'$Y \,\geq\, 4$', fontsize=16)
+    probs = probs[4:]
+    plt.bar(np.arange(4*width, 11 * width, width), probs, width, align='center', alpha=0.3, color='red')
+
     plt.show()
+
 
 def std_normal():
     SAMPLES = 10 ** 5
@@ -35,14 +45,24 @@ def std_normal():
     plt.plot(x, y)
 
     plt.title("Probability density function for standard normal")
-    plt.xlabel(r'$x$')
-    plt.ylabel(r'$p.d.f.(x)$')
+    plt.xlabel(r'$x$', fontsize=16)
+    plt.ylabel(r'$p.d.f.(x)$', fontsize=16)
     plt.ylim((0, 0.45))
     ax = plt.gca()
     ax.fill_between(x, y, facecolor='blue', alpha=0.5)
+    # Decision boundary
+    plt.axvline(x=1, color='r')
+    plt.text(1.1, 0.32727, r'$Y \,\geq\, 1$', fontsize=16)
+    pairs = zip(x, y)
+    pairs = filter(lambda p: p[0] >= 1, pairs)
+    x = [p[0] for p in pairs]
+    y = [p[1] for p in pairs]
+    ax.fill_between(x, y, facecolor='red', alpha=0.3)
+
     plt.show()
 
-geometric(0.5)
-std_normal()
 
+if __name__ == '__main__':
+    geometric(0.5)
+    std_normal()
 
