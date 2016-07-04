71b1aa9522f8573d7a26acc437c6faf19e4918cf
Alex Irpan <xelanapri@gmail.com>
2016-07-03 23:58:48 -0700

    Moving scaling to earlier to get correctly scaled covariance

--- _scripts/fruit_grapher.py
+++ _scripts/fruit_grapher.py
@@ -111,12 +111,8 @@ def replace_nan_and_get_avg(data, categories):
 averages = replace_nan_and_get_avg(data, categories)
 
 
-def show_plot(averages, stretch=True):
-    fig = plt.figure()
-
-    img = plt.imread('xkcdemptyfruit.png')
-    plt.imshow(img)
-    # Coordinates of key locations in image
+def transform_data(data, averages, categories):
+    """Transforms the data into plotted (x, y) coordinates."""
     LEFT = (19, 288)
     CENTER = (339, 288)
     RIGHT = (659, 288)
@@ -128,49 +124,84 @@ def show_plot(averages, stretch=True):
     # This is (0, 0) in fruit space
     BOT_LEFT = (LEFT[0] - EASE, BOTTOM[1] - TASTE)
 
-    if stretch:
-        min_ease = min(t[0] for t in averages.values())
-        max_ease = max(t[0] for t in averages.values())
-        min_taste = min(t[1] for t in averages.values())
-        max_taste = max(t[1] for t in averages.values())
-        taste_scale = (max_ease - min_ease) / (max_taste - min_taste)
-    else:
-        taste_scale = 1
+    # Manually strecth the taste scale to make the resulting graph
+    # less compact
+    min_ease = min(t[0] for t in averages.values())
+    max_ease = max(t[0] for t in averages.values())
+    min_taste = min(t[1] for t in averages.values())
+    max_taste = max(t[1] for t in averages.values())
+    taste_scale = (max_ease - min_ease) / (max_taste - min_taste)
     print 'Scaling tastes by %f' % taste_scale
-    avg_ease = sum(t[0] for t in averages.values()) / len(averages)
+
+    num_replies = data.shape[0]
+    num_fruit = data.shape[1]
     avg_taste = sum(t[1] for t in averages.values()) / len(averages)
-    print 'Average of data is (%f, %f)' % (avg_ease, avg_taste)
+
+    for j in xrange(num_replies):
+        for i in xrange(num_fruit):
+            data[j, i, 0] = BOT_LEFT[0] + EASE * data[j, i, 0]
+            taste = data[j, i, 1]
+            taste = (taste - avg_taste) * taste_scale + avg_taste
+            data[j, i, 1] = BOT_LEFT[1] + TASTE * taste
+    print 'Scaled data to match coordinates.'
+
+    plotted_avgs = dict()
+    for i in range(num_fruit):
+        fruit_name = categories[i].rsplit(' ', 1)[0][:-1]
+        plotted_avgs[fruit_name] = [0, 0]
+        for dim in (0, 1):
+            fruit_ease = data[:, i, dim]
+            n_ease = (~np.isnan(fruit_ease)).sum()
+            total = fruit_ease.sum()
+            avg = float(total) / n_ease
+            plotted_avgs[fruit_name][dim] = avg
+    print 'Computed scaled averages'
+    return plotted_avgs
+
+
+plotted_avgs = transform_data(data, averages, categories)
+
+
+def fit_gaussian(data, averages, categories):
+    """ Fits a Gaussian distribution to the data through MLE. """
+    num_replies = data.shape[0]
+    num_fruit = data.shape[1]
+    covariances = dict()
+
+    for i in range(num_fruit):
+        fruit_name = categories[i].rsplit(' ', 1)[0][:-1]
+        covariances[fruit_name] = np.zeros((2,2))
+        avg = np.array(averages[fruit_name])
+        for j in xrange(num_replies):
+            v = (data[j, i] - avg).reshape((2,1))
+            covariances[fruit_name] += np.dot(v, v.reshape(1,2))
+        covariances[fruit_name] /= num_replies
+
+    return covariances
+
+
+covariances = fit_gaussian(data, plotted_avgs, categories)
+
+
+def show_plot(averages):
+    fig = plt.figure()
+
+    img = plt.imread('xkcdemptyfruit.png')
+    plt.imshow(img)
 
     fruits = []
     ease_coor = []
     taste_coor = []
     for fruit in averages.keys():
         fruits.append(fruit)
-        ease = averages[fruit][0]
-        ease_coor.append(BOT_LEFT[0] + ease * EASE)
-        taste = averages[fruit][1]
-        taste = (taste - avg_taste) * taste_scale + avg_taste
-        taste_coor.append(BOT_LEFT[1] + taste * TASTE)
+        ease_coor.append(averages[fruit][0])
+        taste_coor.append(averages[fruit][1])
 
     plt.scatter(ease_coor, taste_coor)
     for i, fruit in enumerate(fruits):
         xy = [ease_coor[i] - 10, taste_coor[i] - 5]
         plt.annotate(s=fruit, xy=xy)
-        add_image(fig, 'fruit-pics/%s.png' % fruit.lower(), xy[0], xy[1])
-    plt.savefig('foo.png')
+    plt.show()
 
 
-def add_image(fig, filename, x, y):
-    # Adds image to (x, y) in the plot
-    if not os.path.isfile(filename):
-        print '%s could not be found' % filename
-        return
-    img = plt.imread(filename)
-    width = img.shape[1]
-    height = img.shape[0]
-    ax_img = fig.add_axes([x, y, width, height])
-    ax_img.imshow(img)
-    print 'Added %s' % filename
-
-
-show_plot(averages, stretch=True)
+show_plot(plotted_avgs)
