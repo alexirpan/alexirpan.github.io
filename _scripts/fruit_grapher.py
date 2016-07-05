7c5c33df669433774a106d89a7b1222334a208aa
Alex Irpan <xelanapri@gmail.com>
2016-07-04 19:10:02 -0700

    Moving the data cleaning code to another file

--- _scripts/fruit_grapher.py
+++ _scripts/fruit_grapher.py
@@ -7,42 +7,14 @@ import matplotlib.patches as patches
 
 
 def read_data():
-    path = os.path.join(os.path.expanduser('~'), 'fruit.csv')
+    path = os.path.join(os.path.expanduser('~'), 'clean_fruit.csv')
     with open(path) as f:
         reader = csv.reader(f, delimiter=",")
         categories = reader.next()
-        categories = categories[1:-2]
         rows = []
         for row in reader:
             rows.append(row)
-        rows = clean_data(rows)
         return categories, rows
-
-
-def clean_data(rows):
-    # Removes duplicate entries. An entry is for sure a duplicate if it uses
-    # the same email. This won't get rid of all entries but I'm assuming
-    # no one tried to seriously attack a survey about fruit
-    dedup_rows = []
-    seen_emails = set()
-    for row in rows:
-        email = row[-1]
-        # First item = timestamp. Last two items = whether to be notified and
-        # email. Don't want either of these anymore, remove before doing
-        # further analysis
-        anon_row = row[1:-2]
-        if email == '':
-            dedup_rows.append(anon_row)
-        elif email not in seen_emails:
-            seen_emails.add(email)
-            dedup_rows.append(anon_row)
-    print 'Removed %d duplicate entries, leaving %d replies' % (
-        len(rows) - len(dedup_rows), len(dedup_rows))
-    # Remove entries with no opinion
-    cleaned_rows = [row for row in dedup_rows if row.count('') < len(row)]
-    print 'Removed %d empty entries, leaving %d replies' % (
-        len(dedup_rows) - len(cleaned_rows), len(cleaned_rows))
-    return cleaned_rows
 
 
 def unique_count(rows):
@@ -306,4 +278,4 @@ def show_plot(data, averages, covariances, ellipse=True, orig_fruit=True):
     plt.show()
 
 
-show_plot(data, plotted_avgs, covariances, ellipse=True, orig_fruit=False)
+show_plot(data, plotted_avgs, covariances, ellipse=False, orig_fruit=True)
